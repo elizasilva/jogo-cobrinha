26d6c6569a89a7dbb6a86996a6d8532f96df48bd
Eliza Maria da Silva <elizamsilva@outlook.com.br>
2022-02-17 11:16:21 -0300

    Teclas e Lopp
    - Setado as teclas para as direções;
    - incrementado o loop ao sair de um dos lados da tela, aparecer no lado oposto.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);    
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -21,15 +21,30 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+function update(event) {
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'up') direction = 'down';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'down') direction = 'up';
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 15*box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16*box;
+    if(snake[0].y > 15*box && direction == 'up') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'down') snake[0].y = 16*box;
+
     criarBG();
     criarCobrinha();
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
     if (direction == "right") snakeX += box;
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY += box;
     if (direction == "down") snakeY -= box;
+    
     snake.pop();
     let newHead = {
         x: snakeX,
